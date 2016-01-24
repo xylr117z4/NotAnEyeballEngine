6d2852ec204e2b6997dbb938462175cf306a2818
Tyler Green <xylr@live.com>
2016-01-23 19:46:46 -0700

    Created Main.cpp
    Added main.cpp which creates a standard sfml window

---
+++ NotAnEyeballEngine/src/main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+	
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+			{
+                window.close();
+			}
+			
+        }
+        
+		window.clear();
+		window.display();
+        
+    }
+    return EXIT_SUCCESS;
+}
